d90f5c155e629dfd280d77482112b92149785e35
thuminh360 <126739832+thuminh360@users.noreply.github.com>
2025-02-23 12:19:52 +0700

    Add files via upload

---
+++ bai4.2.py
@@ -0,0 +1,16 @@
+def mahoa(message, p, q, e):
+    n = p * q
+    z = (p - 1) * (q - 1)
+    L = []
+    for char in message:
+        M = ord(char)  # Chuyển ký tự thành mã ASCII
+        C = pow(M, e, n)  # Mã hóa
+        L.append(C)
+    return L
+
+p = 17
+q = 23
+e = 5
+message = "NguyenThiThuMinh"
+L = mahoa(message, p, q, e)
+print("Sau khi ma hoa:", L)
